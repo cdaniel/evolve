46464eec67494cd7b5044219903e2e203b4a1b64
Benjamin Mosior <bemosior@gmail.com>
2019-01-26 23:48:04 -0500

    import evolutionary characteristics

--- import.js
+++ import.js
@@ -2,6 +2,35 @@ var gsjson = require('google-spreadsheet-to-json');
 var beautify = require('js-beautify');
 var fs = require('fs');
 
+// Evolutionary Characteristics
+gsjson({
+  spreadsheetId: '1iUjZTCCv1KsgQ5VNohtU1c3BpW7pwh7N_FDgJimjHF8',
+  worksheet: 'Evolutionary Characteristics'
+})
+.then(function(result) {
+  // console.log(result.length);
+  // console.log(result);
+  var evolutionary_characteristics = {};
+  result.forEach(function(element){
+    if(!(element['category'] in evolutionary_characteristics)) {
+      evolutionary_characteristics[element['category']] = [];
+    }
+    evolutionary_characteristics[element['category']].push({
+      name: element['name'],
+      genesis: element['genesis'],
+      custom: element['custom'],
+      'product(+rental)': element['product(+rental)'],
+      'commodity(+utility)': element['commodity(+utility)']
+    });
+  });
+  var json = beautify(JSON.stringify(evolutionary_characteristics), { indent_size: 2, space_in_empty_paren: true });
+  fs.writeFileSync('data/evolutionary_characteristics.json', json);
+})
+.catch(function(err) {
+  console.log(err.message);
+  console.log(err.stack);
+});
+
 // Climatic Patterns
 gsjson({
     spreadsheetId: '1iUjZTCCv1KsgQ5VNohtU1c3BpW7pwh7N_FDgJimjHF8',
